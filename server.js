29ff066b4af25107c3b44ef3f1a9686365e8e560
Андрей Корешков <akoreshkov@alfabank.ru>
2020-04-16 22:57:22 +0300

    change port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(bodyParser.json({ type: 'application/json' }))
 
 server.use('/api', apiRouter);
 
-server.listen(3000, (err) => {
+server.listen(80, (err) => {
     if (err) {
         console.log(err);
     } else {
